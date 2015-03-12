91d6a3055df02f2d405f909120577fa05959ab7d
user <user@gmail.com>
2015-03-13 11:14:59 +1300

    Commiting the player instructions

--- CricketCrickey!/CricketCrickey!/View.cs
+++ CricketCrickey!/CricketCrickey!/View.cs
@@ -8,5 +8,25 @@ namespace CricketCrickey_
 {
     class View
     {
+        
+        public void PlayerInstructions()
+        {
+
+            Console.WriteLine("Please read the instructions below to understand how this game works, hit enter to flow through");
+            Console.ReadLine();
+            Console.WriteLine("You, the player have flipped and are now batting");
+            Console.ReadLine();
+            Console.WriteLine("You will enter a number between 1 and 4, this will correspond to the type of shot you produce");
+            Console.Clear();
+            Console.WriteLine("A 1 will produce a straight drive, hitting down the ground with power");
+            Console.ReadLine();
+            Console.WriteLine("A 2 will produce a cover drive, a classy cricket shot, Hit it BRUDDAA!!!");
+            Console.ReadLine();
+            Console.WriteLine("A 3 will produce a reverse sweep, a risky shot, but one that gets the crowd going");
+            Console.ReadLine();
+            Console.WriteLine("A 4 will produce a slog, who do you you think you are, Brendon McCullum?");
+
+        }
+
     }
 }
