518c5d75afda76897511b9573542bfd1754925ae
user <user@gmail.com>
2015-03-13 10:56:54 +1300

    The frame

---
+++ CricketCrickey!/CricketCrickey!/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketCrickey_
+{
+    class Controller
+    {
+
+        Model model = new Model();
+
+        View view = new View();
+
+    }
+}
---
+++ CricketCrickey!/CricketCrickey!/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketCrickey_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Controller controller = new Controller();
+
+        }
+    }
+}
